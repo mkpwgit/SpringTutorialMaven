c1eefd6e2f14005e650bbc48f93c76598f5de04c
vgerton <191219k1>
2013-11-01 17:40:55 +0300

    add message source support

--- src/main/java/spring/tutorial/maven/domain/Circle.java
+++ src/main/java/spring/tutorial/maven/domain/Circle.java
@@ -4,7 +4,10 @@ package spring.tutorial.maven.domain;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Required;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
+
+import java.util.Locale;
 
 @Component
 public class Circle implements Shape{
@@ -12,6 +15,8 @@ public class Circle implements Shape{
     @Autowired
     @Qualifier("pointA")
     private Point center;
+    @Autowired
+    private MessageSource messageSource;
 
     public Point getCenter() {
         return center;
@@ -25,5 +30,6 @@ public class Circle implements Shape{
     @Override
     public void draw() {
         System.out.println("Draw circle with the center: "+center);
+        System.out.println(messageSource.getMessage("greeting", null, "Default greeting!", new Locale("ru")));
     }
 }
--- src/main/java/spring/tutorial/maven/domain/Point.java
+++ src/main/java/spring/tutorial/maven/domain/Point.java
@@ -1,5 +1,10 @@
 package spring.tutorial.maven.domain;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+
+import java.util.Locale;
+
 /**
  * @author Mikalai Kisel
  */
@@ -7,6 +12,9 @@ public class Point {
 
     private int x;
     private int y;
+
+    @Autowired
+    private MessageSource messageSource;
 
     public int getX() {
         return x;
@@ -26,9 +34,6 @@ public class Point {
 
     @Override
     public String toString() {
-        return "Point{" +
-                "x=" + x +
-                ", y=" + y +
-                '}';
+        return messageSource.getMessage("drawing.point", new Object[] {x, y}, "Default drawing point message!", new Locale("ru"));
     }
 }
