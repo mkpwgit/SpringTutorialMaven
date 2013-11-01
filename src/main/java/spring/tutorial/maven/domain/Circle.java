01046e7f6e2bbac501e735a2aa37b9a8a5fa1447
vgerton <191219k1>
2013-11-01 15:42:25 +0300

    add annotations

--- src/main/java/spring/tutorial/maven/domain/Circle.java
+++ src/main/java/spring/tutorial/maven/domain/Circle.java
@@ -1,14 +1,23 @@
 package spring.tutorial.maven.domain;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Circle implements Shape{
 
+    @Autowired
+    @Qualifier("pointA")
     private Point center;
 
     public Point getCenter() {
         return center;
     }
 
+//    @Required
     public void setCenter(Point center) {
         this.center = center;
     }
